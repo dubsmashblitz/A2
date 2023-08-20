a8df72bd8c7f85571811a3f608c7ee31fb0d50ce
dubsmashblitz <93750257+dubsmashblitz@users.noreply.github.com>
2023-08-21 00:57:18 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,7 +82,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={removeTags(post.title)} />
+				
 				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:image:width" content="600" />

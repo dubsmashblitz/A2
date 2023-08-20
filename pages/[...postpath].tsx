9d2f0c54b1312291fdc2f654ff3c366ffea2a5a2
dubsmashblitz <93750257+dubsmashblitz@users.noreply.github.com>
2023-08-21 01:11:44 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -97,14 +97,14 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title>{post.title}</title>
+				<title> </title>
 			</Head>
 			<div className="post-container">
-				<h1>{post.title}</h1>
+				
 				<img
 					src={post.featuredImage.node.sourceUrl}
 				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
+				
 			</div>
 		</>
 	);
